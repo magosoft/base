79ef5801754f4a7092e5a1647f6f4793cb2fc103
Guido Caceres <magosoft@users.noreply.github.com>
2024-07-01 22:58:04 -0400

    layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'base';
+  constructor() {
+    let r = {
+      uno: 'u',
+      dos: 'tipo',
+      tres: {
+        tres_uno: '',
+        tres_dos: '',
+      },
+      cuatro: '',
+      mil: '',
+    };
+    for (var key in r) {
+
+      console.log(key);
+      console.log();
+    }
+  }
 }
--- src/app/models/field.ts
+++ src/app/models/field.ts
@@ -1,2 +1,8 @@
 export class Field {
+  constructor(
+    public label: string,
+    public dbType: string = 'varchar',
+    public len: number,
+    public required: boolean = false
+  ) {}
 }
